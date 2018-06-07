fb5777d03113a594e73d7abc678f71088b5f20e6
Stacionarni-PC <p3ter.sik@gmail.com>
2018-06-07 11:11:33 +0200

    Created measurement struct to hold info about the measurement

--- Assets/DistanceMeasure/DistanceMeasureManager.cs
+++ Assets/DistanceMeasure/DistanceMeasureManager.cs
@@ -1,18 +1,22 @@
-﻿using System.Collections;
+﻿using Assets.DistanceMeasure;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DistanceMeasureManager : MonoBehaviour
 {
+    public Text distance;
+
+    int numLines = 1;
     bool awaitingFirstInput = true;
     bool awaitingSecondInput = false;
     Vector3 firstTouchPosition;
     Vector3 secondTouchPosition;
 
-    private void Start()
-    {
-        gameObject.AddComponent<LineRenderer>();
-    }
+    //TODO scriptObject za seznam meritev, da lahko dostopam iz več koncev
+    List<GameObject> lineRendererContainers = new List<GameObject>();
+    List<Measurement> measurements = new List<Measurement>();
 
     private void Update()
     {
@@ -51,11 +55,20 @@ public class DistanceMeasureManager : MonoBehaviour
 
     private void CreateLineRenderer()
     {
-        LineRenderer lr = GetComponent<LineRenderer>();
+        GameObject go = new GameObject();
+        go.transform.parent = gameObject.transform;
+        go.name = $"Measurement_{numLines++}";
+        lineRendererContainers.Add(go);
+        LineRenderer lr = go.AddComponent<LineRenderer>();
+        lr.enabled = true;
         lr.positionCount = 2;
         lr.startWidth = 0.2f;
         lr.endWidth = lr.startWidth;
         Vector3[] positions = { firstTouchPosition, secondTouchPosition };
         lr.SetPositions(positions);
+        float distance = Vector3.Distance(firstTouchPosition, secondTouchPosition);
+        this.distance.text = distance.ToString();
+        Measurement createdMeasure = new Measurement(distance, go.name);
+        measurements.Add(createdMeasure);
     }
 }
---
+++ Assets/DistanceMeasure/Measurement.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.DistanceMeasure
+{
+    public struct Measurement
+    {
+        public Measurement(float distance, string label)
+        {
+            Distance = distance;
+            Label = label;
+        }
+
+        public float Distance { get; }
+        public string Label { get; set; }
+    }
+}
